3bb9e744fd603e225e4e75bc7a463bf60782ad6d
ronxldz <erik.p.1208@icloud.com>
2023-07-16 16:55:42 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import ContactList from './components/ContactList'
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+      <ContactList />
     </>
   )
 }
