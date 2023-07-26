038e1fc254c7a6496dc4786edd0ba942fba8013d
ronxldz <erik.p.1208@icloud.com>
2023-07-25 21:29:46 -0400

    fetched the contact list

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,15 +1,30 @@
 import React from "react"; 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import ContactRow from "./ContactRow";
 
-const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-  ];
+// const dummyContacts = [
+//     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+//     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+//     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+//   ];
   
   export default function ContactList() {
-    const [contacts, setContacts] = useState(dummyContacts);
+    const [contacts, setContacts] = useState([]);
+    console.log(contacts)
+    useEffect(() => {
+        async function fetchContacts() {
+          try {
+            const response = await fetch(
+              "https://jsonplaceholder.typicode.com/users"
+            );
+            const result = await response.json();
+            setContacts(result);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchContacts();
+      }, []);
   
     return (
       <table>
